41c992a358d2bb0af74a11e7b7d1cb6782b6455e
Mark Larah <mark@larah.me>
2025-12-26 21:45:26 -0600

    fix launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+# https://stackoverflow.com/a/246128/4396258
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WINDOW_ID=$(uuidgen)
+
 open -na ghostty.app --args -e bash -c "\
   aerospace layout floating && \
-  ~/apps/spacelist/sl --id=\"$WINDOW_ID\" \
+  spacelist --id=\"$WINDOW_ID\" \
 "
 
 success=false
 for i in {1..5}; do
-  output=$(./center.swift "spacelist-$WINDOW_ID" 2>&1)
+  output=$("${DIR}/center.swift" "spacelist-$WINDOW_ID" 2>&1)
   if [ $? -eq 0 ]; then
     success=true
     break
